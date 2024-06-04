2aa45a89820c8cc88b0b06e9103f0ac174579315
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-06-05 01:54:40 +0600

    Create a route for package usign ID

--- index.js
+++ index.js
@@ -143,8 +143,15 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/packege",  async (req, res) => {
+    app.get("/packege", async (req, res) => {
       const result = await packegCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/packageDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await packegCollection.findOne(query);
       res.send(result);
     });
 
